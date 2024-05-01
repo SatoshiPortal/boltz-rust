cae7b4ee1287fdc895b2fe2dbf894d9e177b7473
i5hi <vishalmenon.92@gmail.com>
2024-05-02 03:34:51 +0530

    Updated boltz.rs to use native-tls for POST requests with rustls as fallback

--- src/swaps/boltz.rs
+++ src/swaps/boltz.rs
@@ -44,6 +44,7 @@ use elements::secp256k1_zkp::Keypair as ZKKeyPair;
 use elements::secp256k1_zkp::Secp256k1 as ZKSecp256k1;
 use lightning_invoice::Bolt11Invoice;
 use serde_json::Value;
+use ureq::AgentBuilder;
 // use reqwest;
 use crate::swaps::bitcoin::BtcSwapScript;
 use crate::swaps::liquid::LBtcSwapScript;
@@ -52,6 +53,7 @@ use serde::Serializer;
 use serde::{Deserialize, Serialize};
 use serde_json;
 use std::str::FromStr;
+use std::sync::Arc;
 
 pub const BOLTZ_TESTNET_URL: &str = "https://testnet.boltz.exchange/api";
 pub const BOLTZ_MAINNET_URL: &str = "https://api.boltz.exchange";
@@ -82,7 +84,21 @@ impl BoltzApiClient {
     /// Make a Post request. Returns the Response
     fn post(&self, end_point: &str, data: Value) -> Result<String, Error> {
         let url = format!("{}/{}", self.base_url, end_point);
-        Ok(ureq::post(&url).send_json(data)?.into_string()?)
+        
+        let response = match native_tls::TlsConnector::new() {
+            // If native_tls is available, use that for TLS
+            // It has better handling of close_notify, which avoids some POST call failures
+            // See https://github.com/SatoshiPortal/boltz-rust/issues/39
+            Ok(tls_connector) => AgentBuilder::new()
+                .tls_connector(Arc::new(tls_connector))
+                .build()
+                .request("POST", &url)
+                .send_json(data)?
+                .into_string()?,
+            // If native_tls is not available, fallback to the default (rustls)
+            Err(_) => ureq::post(&url).send_json(data)?.into_string()?,
+        };
+        Ok(response)
     }
     /// In order to create a swap, one first has to know which pairs are supported and what kind of rates, limits and fees are applied when creating a new swap.
     /// The following call returns this information.
