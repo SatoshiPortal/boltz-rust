640def03cf7752286a321ed7442a8a3c52852644
ok300 <106775972+ok300@users.noreply.github.com>
2024-05-21 10:39:52 +0000

    Update src/swaps/boltz.rs
    Co-authored-by: michael1011 <me@michael1011.at>

--- src/swaps/boltz.rs
+++ src/swaps/boltz.rs
@@ -319,8 +319,8 @@ pub enum SubSwapStates {
     /// Initial state of the swap; optionally the initial state can also be `invoice.set` in case
     /// the invoice was already specified in the request that created the swap.
     Created,
-    /// The lockup transaction is found in the mempool, meaning the user is sending funds to the
-    /// lockup chain address.
+    /// The lockup transaction waas found in the mempool, meaning the user sent funds to the
+    /// lockup address.
     TransactionMempool,
     /// The lockup transaction was included in a block.
     TransactionConfirmed,
