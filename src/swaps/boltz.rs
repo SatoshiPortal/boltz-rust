af8a005b073665ffeba458e9b26e882589352f3b
Ross Savage <hello@satimoto.com>
2024-08-09 09:06:54 +0200

    Add description hash to reverse request

--- src/swaps/boltz.rs
+++ src/swaps/boltz.rs
@@ -700,6 +700,8 @@ pub struct CreateReverseRequest {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
+    pub description_hash: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub address: Option<String>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub address_signature: Option<String>,
