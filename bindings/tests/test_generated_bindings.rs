6c91ab3e0f4db24ec286276a8341eb75c910cb9a
jackstar12 <jkranawetter05@gmail.com>
2025-07-31 23:18:03 +0200

    refactor: run bindings tests in serial

--- bindings/tests/test_generated_bindings.rs
+++ bindings/tests/test_generated_bindings.rs
@@ -1,3 +1,28 @@
-uniffi::build_foreign_language_testcases!("tests/bindings/reverse.py",);
-uniffi::build_foreign_language_testcases!("tests/bindings/chain.py",);
-uniffi::build_foreign_language_testcases!("tests/bindings/submarine.py",);
+use serial_test::serial;
+
+fn run_test(script: &str) {
+    uniffi::python_test::run_test(
+        std::env!("CARGO_TARGET_TMPDIR"),
+        std::env!("CARGO_PKG_NAME"),
+        script,
+    )
+    .unwrap();
+}
+
+#[test]
+#[serial]
+fn reverse() {
+    run_test("tests/bindings/reverse.py");
+}
+
+#[test]
+#[serial]
+fn chain() {
+    run_test("tests/bindings/chain.py");
+}
+
+#[test]
+#[serial]
+fn submarine() {
+    run_test("tests/bindings/submarine.py");
+}
