60d2076bcaaedf72bdf5ca954fabfcd793d21980
Riccardo Casatta <riccardo@casatta.it>
2026-03-31 10:32:17 +0200

    use existing bech32 dep (remove older one)

--- src/util/bolt12.rs
+++ src/util/bolt12.rs
@@ -1,18 +1,19 @@
-use bech32::FromBase32;
+use bitcoin::bech32::{primitives::decode::CheckedHrpstring, NoChecksum};
 use lightning::offers::invoice::Bolt12Invoice;
 
 use crate::error::Error;
 
 pub const BECH32_BOLT12_INVOICE_HRP: &str = "lni";
 pub fn parse_bolt12_invoice(bolt12_invoice: &str) -> Result<Bolt12Invoice, Error> {
-    // TODO: for some reason, upstream Bolt12Invoice::from_str is not supported, thus we do it manually
-    let (hrp, data) = bech32::decode_without_checksum(bolt12_invoice)
-        .map_err(|e| Error::Generic(e.to_string()))?;
-    if hrp.as_str() != BECH32_BOLT12_INVOICE_HRP {
-        return Err(Error::Generic(format!("invalid hrp: {hrp}")));
+    let dec = match CheckedHrpstring::new::<NoChecksum>(bolt12_invoice) {
+        Ok(dec) => dec,
+        Err(err) => return Err(Error::Generic(format!("{err:?}"))),
+    };
+    if dec.hrp().to_lowercase() != BECH32_BOLT12_INVOICE_HRP {
+        return Err(Error::Generic(format!("invalid hrp: {}", dec.hrp())));
     }
 
-    let data = Vec::<u8>::from_base32(&data).map_err(|e| Error::Generic(e.to_string()))?;
+    let data = dec.byte_iter().collect::<Vec<_>>();
     lightning::offers::invoice::Bolt12Invoice::try_from(data)
-        .map_err(|e| Error::Generic(format!("{e:?}")))
+        .map_err(|err| Error::Generic(format!("{err:?}")))
 }
